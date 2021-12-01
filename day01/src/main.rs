d55f84cd632a1349558a2cb1d4870a3c51292022
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2021-12-01 10:59:37 +0000

    Clean Day 1 up a bit

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,37 +1,37 @@
+use std::num::ParseIntError;
+
 use itertools::Itertools;
 
-fn task1() {
-    let num_increases = include_str!("input.txt")
-        .lines()
-        .map(str::parse::<i32>)
-        .map(Result::unwrap)
+fn task1(input: impl Iterator<Item = i32>) -> i32 {
+    input
         .tuple_windows()
-        .fold(0, |inc, (a, b)| if b > a { inc + 1 } else { inc });
-
-    println!(
-        "Task 1: {} measurements were larger than the previous measurement",
-        num_increases
-    );
+        .fold(0, |inc, (a, b)| if b > a { inc + 1 } else { inc })
 }
 
-fn task2() {
-    let num_increases = include_str!("input.txt")
-        .lines()
-        .map(str::parse::<i32>)
-        .map(Result::unwrap)
+fn task2(input: impl Iterator<Item = i32>) -> i32 {
+    input
         // This is the same but I added these two lines to sum up the windows
         .tuple_windows()
         .map(|(a, b, c)| a + b + c)
         .tuple_windows()
-        .fold(0, |inc, (a, b)| if b > a { inc + 1 } else { inc });
+        .fold(0, |inc, (a, b)| if b > a { inc + 1 } else { inc })
+}
+
+fn main() -> Result<(), ParseIntError> {
+    let input: Vec<i32> = include_str!("input.txt")
+        .lines()
+        .map(str::parse::<i32>)
+        .try_collect()?;
+
+    println!(
+        "Task 1: {} measurements were larger than the previous measurement",
+        task1(input.iter().copied())
+    );
 
     println!(
         "Task 2: {} sums were larger than the previous sum",
-        num_increases
+        task2(input.iter().copied())
     );
-}
 
-fn main() {
-    task1();
-    task2();
+    Ok(())
 }
