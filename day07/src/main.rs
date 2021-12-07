0dd905aa02afed11754dfe936afca2f619c5f0e7
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2021-12-07 12:03:11 +0000

    Refactor Day 7 to use the mean

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -5,6 +5,7 @@ fn task1_dist(crab_pos: i32, pos: i32) -> i32 {
 fn task2_dist(crab_pos: i32, pos: i32) -> i32 {
     let dist = (crab_pos - pos).abs();
 
+    // Triangle numbers
     (dist * (dist + 1)) / 2
 }
 
@@ -15,26 +16,31 @@ fn main() {
         .map(Result::unwrap)
         .collect();
 
-    // The median of the positions is the optimal position
+    // The median of the positions is the optimal position.
+    // This is because the median minimises the distances to all of the points.
     positions.sort_unstable();
-    let optimal_pos = positions[(positions.len() / 2) - 1];
+    let median_pos = positions[(positions.len() / 2) - 1];
 
     let fuel_task1: i32 = positions
         .iter()
-        .map(|&crab_pos| task1_dist(crab_pos, optimal_pos))
+        .map(|&crab_pos| task1_dist(crab_pos, median_pos))
         .sum();
 
     println!("Task 1: {}", fuel_task1);
 
-    // I just brute forced Task 2 lol
-    let &min = positions.iter().min().unwrap();
-    let &max = positions.iter().max().unwrap();
+    // The mean minimises the squared distances to all of the points.
+    // The distance to a point is (n^2 + n)/2, so we actually need to minimise
+    // n^2 + n, but our data set is small enough that we can just find the mean
+    // to minimise n^2 and check the floor and the ceiling.
+    let mean_floor = positions.iter().sum::<i32>() / (positions.len() as i32);
+    let mean_ceil = mean_floor + 1;
 
-    let fuel_task2: i32 = (min..max)
-        .map(|pos| {
+    let fuel_task2: i32 = [mean_floor, mean_ceil]
+        .iter()
+        .map(|&pos| {
             positions
                 .iter()
-                .map(move |&crab_pos| task2_dist(crab_pos, pos))
+                .map(|&crab_pos| task2_dist(crab_pos, pos))
                 .sum()
         })
         .min()
